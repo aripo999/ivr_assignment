8180c38e2450b4ece4ca894f79e9a4dada0faa33
Julia Töws <s8jutoew@stud.uni-saarland.de>
2021-11-24 22:56:41 +0000

    make joint estimation work for all joints (at least individually)

--- src/vision_1.py
+++ src/vision_1.py
@@ -21,6 +21,8 @@ class JointEstimation1:
         joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
         joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
 
+        joint2_est_pub = rospy.Publisher("/robot/joint2_estimation", Float64, queue_size=10)
+        joint3_est_pub = rospy.Publisher("/robot/joint3_estimation", Float64, queue_size=10)
         joint4_est_pub = rospy.Publisher("/robot/joint4_estimation", Float64, queue_size=10)
 
         camera_1_sub = rospy.Subscriber("/camera1/robot/image_raw", Image, self.callback1)
@@ -32,6 +34,8 @@ class JointEstimation1:
         self.cv_image_x = np.array([])
         self.cv_image_y = np.array([])
 
+        self.pixel2meter_factor = None
+
         t0 = rospy.get_time()
         while not rospy.is_shutdown():
             cur_time = np.array([rospy.get_time()]) - t0
@@ -41,10 +45,10 @@ class JointEstimation1:
             joint4_value = np.pi/2 * np.sin(np.pi / 18 * cur_time)
 
             joint2 = Float64()
-            joint2.data = 0
+            joint2.data = joint2_value
 
             joint3 = Float64()
-            joint3.data = 0
+            joint3.data = joint3_value
 
             joint4 = Float64()
             joint4.data = joint4_value
@@ -52,11 +56,19 @@ class JointEstimation1:
             joint2_pub.publish(joint2)
             joint3_pub.publish(joint3)
             joint4_pub.publish(joint4)
+
             if self.cv_image_x.size != 0 and self.cv_image_y.size != 0:
                 cv2.imshow('window1', self.cv_image_x)
                 cv2.waitKey(1)
                 cv2.imshow('window2', self.cv_image_y)
                 cv2.waitKey(1)
+
+                joint2_est = Float64()
+                joint2_est.data = self.detect_joint_angles(self.cv_image_x, self.cv_image_y)[0]
+                joint2_est_pub.publish(joint2_est)
+                joint3_est = Float64()
+                joint3_est.data = self.detect_joint_angles(self.cv_image_x, self.cv_image_y)[1]
+                joint3_est_pub.publish(joint3_est)
                 joint4_est = Float64()
                 joint4_est.data = self.detect_joint_angles(self.cv_image_x, self.cv_image_y)[2]
                 joint4_est_pub.publish(joint4_est)
@@ -130,16 +142,12 @@ class JointEstimation1:
         dist = np.sum((circle1Pos - circle2Pos)**2)
         return 4 / np.sqrt(dist)
 
-    def angle(self, x, y, z):
-        right = x - y
-        left = z - y
-        cosine_angle = np.dot(right, left) / (np.linalg.norm(right) * np.linalg.norm(left))
-        factor = -1 if right[0] * left[1] - right[1] * left[0] > 0 else 1
-        return factor * np.arccos(cosine_angle)
 
     # Calculate the relevant joint angles from the image
     def detect_joint_angles(self,image_1, image_2):
-        a = self.pixel2meter(image_1)
+        if self.pixel2meter_factor is None:
+            self.pixel2meter_factor = self.pixel2meter(image_1)
+        a = self.pixel2meter_factor
 
         image_x = cv2.cvtColor(image_1, cv2.COLOR_BGR2HSV)
         image_y = cv2.cvtColor(image_2, cv2.COLOR_BGR2HSV)
@@ -147,44 +155,35 @@ class JointEstimation1:
         # Obtain the centre of each coloured blob
         green_x = self.detect_green(image_x)
         green_y = self.detect_green(image_y)
-        center1 = 0 if green_y is None else a * green_y[0]
-        center2 = 0 if green_x is None else a * green_x[0]
-        center3 = a * green_x[1] if green_y is None else a * green_y[1]
-        center = np.array([center1, center2, center3])
+        green1 = 0 if green_y is None else a * green_y[0]
+        green2 = 0 if green_x is None else a * green_x[0]
+        green3 = a * green_x[1] if green_y is None else a * green_y[1]
 
         yellow_x = self.detect_yellow(image_x)
         yellow_y = self.detect_yellow(image_y)
-        circle1Pos1 = 0 if yellow_y is None else a * yellow_y[0]
-        circle1Pos2 = 0 if yellow_x is None else a * yellow_x[0]
-        circle1Pos3 = a * yellow_x[1] if yellow_y is None else a * yellow_y[1]
-        circle1Pos = np.array([circle1Pos1, circle1Pos2, circle1Pos3])
+        yellow1 = green1 if yellow_y is None else a * yellow_y[0]
+        yellow2 = green2 if yellow_x is None else a * yellow_x[0]
+        yellow3 = a * yellow_x[1] if yellow_y is None else a * yellow_y[1]
 
         blue_x = self.detect_blue(image_x)
         blue_y = self.detect_blue(image_y)
-        circle2Pos1 = 0 if blue_y is None else a * blue_y[0]
-        circle2Pos2 = 0 if blue_x is None else a * blue_x[0]
-        circle2Pos3 = a * blue_x[1] if blue_y is None else a * blue_y[1]
-        circle2Pos = np.array([circle2Pos1, circle2Pos2, circle2Pos3])
+        blue1 = yellow1 if blue_y is None else a * blue_y[0]
+        blue2 = yellow2 if blue_x is None else a * blue_x[0]
+        blue3 = a * blue_x[1] if blue_y is None else a * blue_y[1]
 
         red_x = self.detect_red(image_x)
         red_y = self.detect_red(image_y)
-        circle3Pos1 = 0 if red_y is None else a * red_y[0]
-        circle3Pos2 = 0 if red_x is None else a * red_x[0]
-        circle3Pos3 = a * red_x[1] if red_y is None else a * red_y[1]
-        circle3Pos = np.array([circle3Pos1, circle3Pos2, circle3Pos3])
-
-        print(center)
-        print(circle1Pos)
-        print(circle2Pos)
-        print(circle3Pos)
+        red1 = blue1 if red_y is None else a * red_y[0]
+        red2 = blue2 if red_x is None else a * red_x[0]
+        red3 = a * red_x[1] if red_y is None else a * red_y[1]
 
         # Solve using trigonometry
-        rad = self.angle(2 * circle1Pos - center, circle2Pos, circle3Pos)
-        print(np.rad2deg(rad))
-        ja1 = np.arctan2(center[0]- circle1Pos[0], center[1] - circle1Pos[1])
-        ja2 = np.arctan2(circle1Pos[0]-circle2Pos[0], circle1Pos[1]-circle2Pos[1]) - ja1
-        ja3 = np.arctan2(circle2Pos[0]-circle3Pos[0], circle2Pos[1]-circle3Pos[1]) - ja2 - ja1
-        return np.array([ja1, ja2, rad])
+        ja2 = - np.arctan2(yellow1 - blue1, yellow3 - blue3)
+        ja3 = np.arctan2(yellow2 - blue2, yellow3 - blue3)
+        ja4 = - np.arctan2(blue1 - red1, blue3 - red3) - ja2
+
+        print(np.rad2deg(ja3))
+        return np.array([ja2, ja3, ja4])
 
     def callback1(self, data):
         # Recieve the image
